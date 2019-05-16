3bf29cc607b39814a5587ae6a5f17de5300b7225
snaupul <45213219+snaupul@users.noreply.github.com>
2019-05-16 00:38:32 -0400

    Add files via upload

---
+++ GPS_New_Final.ino
@@ -0,0 +1,163 @@
+ 
+#include <Adafruit_GPS.h> //Load the GPS Library. Make sure you have installed the library form the adafruit site above
+#include <SoftwareSerial.h> //Load the Software Serial Library. This library in effect gives the arduino additional serial ports
+#include <LiquidCrystal.h>
+
+int rs = 8;
+int en = 9;
+int d4 = 10;
+int d5 = 11;
+int d6 = 12;
+int d7 = 13;
+
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+SoftwareSerial mySerial(3, 2); //Initialize SoftwareSerial, and tell it you will be connecting through pins 2 and 3
+Adafruit_GPS GPS(&mySerial); //Create GPS object
+
+String NMEA1;  //We will use this variable to hold our first NMEA sentence
+String NMEA2;  //We will use this variable to hold our second NMEA sentence
+
+double x,y;
+long a,b,c,d;
+long latdegree,latmin,latsec;
+long londegree,lonmin,lonsec;
+char e;       //Used to read the characters spewing from the GPS module
+
+void setup()  
+{
+  Serial.begin(115200);  //Turn on the Serial Monitor
+  GPS.begin(9600);       //Turn GPS on at baud rate of 9600
+  GPS.sendCommand("$PGCMD,33,0*6D"); // Turn Off GPS Antenna Update
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Tell GPS we want only $GPRMC and $GPGGA NMEA sentences
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);  // 1 Hz update rate
+  lcd.begin(16,2);
+
+  
+  delay(1000);  //Pause
+}
+
+
+void loop()                     // run over and over again
+{
+readGPS();  //This is a function we define below which reads two NMEA sentences from GPS
+
+  
+//Serial.print("Latitude ==");
+x=GPS.latitude;
+a=x*100;
+latsec=a%100;
+latsec=latsec/100.0*60.0;
+a=a/100;
+latmin=a%100;
+a=a/100;
+latdegree=a%100;
+
+lcd.setCursor(0,0);
+lcd.print("Lat= ");
+
+
+//lcd.setCursor(5,0);
+lcd.print(latdegree);
+lcd.print(" ");
+//lcd.setCursor(9,0);
+lcd.print(latmin);
+lcd.print(" ");
+//lcd.setCursor(13,0);
+lcd.print(latsec);
+lcd.print(" ");
+//lcd.setCursor(15,0);
+lcd.print(GPS.lat);
+lcd.print(" ");
+
+Serial.println("Latitude Degree are ");
+Serial.println(latdegree);
+Serial.println("Latitude Minutes are ");
+Serial.println(latmin);
+Serial.println("Latitude Seconds are ");
+Serial.println(latsec);
+Serial.print(GPS.lat);
+
+
+
+
+y=GPS.longitude;
+a=y*100;
+lonsec=a%100;
+lonsec=lonsec/100.0*60.0;
+a=a/100;
+lonmin=a%100;
+a=a/100;
+londegree=a%100;
+
+lcd.setCursor(0,1);
+lcd.print("Lon= ");
+
+//lcd.setCursor(5,1);
+lcd.print(londegree);
+lcd.print(" ");
+//lcd.setCursor(9,1);
+lcd.print(lonmin);
+lcd.print(" ");
+//lcd.setCursor(13,1);
+lcd.print(lonsec);
+lcd.print(" ");
+//lcd.setCursor(15,1);
+lcd.print(GPS.lon);
+lcd.print(" ");
+
+delay(1000);
+lcd.clear();
+
+Serial.println("Longitude Degree are ");
+Serial.println(londegree);
+Serial.println("Longitude Minutes are ");
+Serial.println(lonmin);
+Serial.println("Longitude Seconds are ");
+Serial.println(lonsec);
+
+//Serial.println(GPS.latitude);
+
+
+//serial.print("Longitude ==");
+//Serial.println(GPS.longitude);
+//Serial.print(GPS.lon);
+//Serial.println(GPS.altitude);
+
+
+//y=GPS.longitude;
+
+//Serial.println(x);
+//Serial.println(y);
+
+
+
+
+}
+void readGPS(){  //This function will read and remember two NMEA sentences from GPS
+  clearGPS();    //Serial port probably has old or corrupt data, so begin by clearing it all out
+  while(!GPS.newNMEAreceived()) { //Keep reading characters in this loop until a good NMEA sentence is received
+  e=GPS.read(); //read a character from the GPS
+  }
+GPS.parse(GPS.lastNMEA());  //Once you get a good NMEA, parse it
+NMEA1=GPS.lastNMEA();      //Once parsed, save NMEA sentence into NMEA1
+while(!GPS.newNMEAreceived()) {  //Go out and get the second NMEA sentence, should be different type than the first one read above.
+  e=GPS.read();
+  }
+GPS.parse(GPS.lastNMEA());
+NMEA2=GPS.lastNMEA();
+  Serial.println(NMEA1);
+  Serial.println(NMEA2);
+  Serial.println("");
+}
+void clearGPS() {  //Since between GPS reads, we still have data streaming in, we need to clear the old data by reading a few sentences, and discarding these
+while(!GPS.newNMEAreceived()) {
+  e=GPS.read();
+  }
+GPS.parse(GPS.lastNMEA());
+while(!GPS.newNMEAreceived()) {
+  e=GPS.read();
+  }
+GPS.parse(GPS.lastNMEA());
+
+}
---
+++ Left_Turn.ino
@@ -0,0 +1,203 @@
+ int tri1 = 23;
+int echopin1 = 22;
+
+int tri2 = 25;
+int echopin2 = 24;
+
+int tri3 = 29;
+int echopin3 = 28;
+
+int tri4 = 33;
+int echopin4 = 32;
+
+long duration1 ,cm1;
+long duration2 ,cm2;
+long duration3 ,cm3;
+long duration4 ,cm4;
+
+int IN1= 2;
+int IN2= 3;
+int EN1= 4;
+int IN3= 5;
+int IN4= 6;
+int EN2= 7;
+int IN5= 36;
+int IN6= 37;
+int IN7= 38;
+int IN8= 39;
+
+//int EN1= 42;
+//int EN2= 43;
+int EN3= 44;
+int EN4= 45;
+
+
+
+
+void setup() {
+
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(IN5,OUTPUT);
+  pinMode(IN6,OUTPUT);
+  pinMode(IN7,OUTPUT);
+  pinMode(IN8,OUTPUT);
+  pinMode(EN1,OUTPUT);
+  pinMode(EN2,OUTPUT);
+  pinMode(EN3,OUTPUT);
+  pinMode(EN4,OUTPUT);
+  
+
+Serial.begin(9600);
+
+
+}
+
+void loop() {
+
+  pinMode(tri1, OUTPUT); // Use to triger the PING sensor
+  digitalWrite(tri1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(tri1, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(tri1, LOW);
+
+  pinMode(echopin1, INPUT);
+  duration1 = pulseIn(echopin1, HIGH);
+
+  cm1 = microsecondsToCentimeters(duration1);  //Function for getting distance in CMS
+  Serial.print(cm1);
+  Serial.print("    ");
+
+  pinMode(tri2, OUTPUT);
+  digitalWrite(tri2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(tri2, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(tri2, LOW);
+
+  pinMode(echopin2, INPUT);
+  duration2 = pulseIn(echopin2, HIGH);
+
+  cm2 = microsecondsToCentimeters(duration2);
+  Serial.print(cm2);
+  Serial.print("    ");
+
+
+pinMode(tri3, OUTPUT);
+  digitalWrite(tri3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(tri3, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(tri3, LOW);
+
+  pinMode(echopin3, INPUT);
+  duration3 = pulseIn(echopin3, HIGH);
+
+ cm3 = microsecondsToCentimeters(duration3);
+  Serial.print(cm3);
+  Serial.print("    ");
+
+  pinMode(tri4, OUTPUT);
+  digitalWrite(tri4, LOW);
+  delayMicroseconds(2);
+  digitalWrite(tri4, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(tri4, LOW);
+
+  pinMode(echopin4, INPUT);
+  duration4 = pulseIn(echopin4, HIGH);
+
+  cm4 = microsecondsToCentimeters(duration4);
+  Serial.print(cm4);
+  Serial.print("    ");
+Serial.println();
+
+  if(cm1 <=20)
+  {
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,50);
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);//function to stop motor
+  analogWrite(EN2,255);
+  delay(5000);
+  
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);  // For max speed.
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  analogWrite(EN2,255);
+  delay(20000);
+  
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);//function to stop motor
+  analogWrite(EN2,50);
+  delay(5000);
+  
+   digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);  // For max speed.
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  analogWrite(EN2,255);
+  delay(20000);
+
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);//function to stop motor
+  analogWrite(EN2,50);
+  delay(5000);
+  
+  
+   digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);  // For max speed.
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  analogWrite(EN2,255);
+  delay(20000);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,50);
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);//function to stop motor
+  analogWrite(EN2,255);
+  delay(5000);
+
+  
+  }
+  else
+  {
+    
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  analogWrite(EN1,255);  // For max speed.
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  analogWrite(EN2,255); // To run motor in forward direction
+  }
+
+ 
+  
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
